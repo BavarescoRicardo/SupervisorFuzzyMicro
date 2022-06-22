89e11f21220ad26394cc2e734a252fbcfc1c961d
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-06-22 01:19:44 -0300

    okei

--- Monitoramento.cs
+++ Monitoramento.cs
@@ -37,8 +37,6 @@ namespace SupervisorFuzzyMicro
             // Verificação e Validação do pacote de dados recebido.
             if (rpm.Length > 4 && rpm[0] == '#' && rpm[1] == '$' && rpm[2] == ':')
             {
-                // return rpm.Substring(2, 10);
-
                 rotacao = (rpm[3] << 8) + (rpm[4]);
 
                 return rotacao.ToString();
